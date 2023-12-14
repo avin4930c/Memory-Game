a0851a7cbb78c3f3c4d3e9aa97c5ac69a5e850d2
Avinash <123456789+avinash4930c@gmail.com>
2023-12-14 23:33:57 +0530

    base js and css structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-
-let cardArray = {};
-
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-  }
-}
-
-function createCard({i, imgUrl, imgName}) {
-  return(
-    <>
-    <div id={i} className="card"><img src={imgUrl}></img><p className="card-name">{imgName}</p></div>
-    </>
-  )
-}
 
 function App() {
 
+  const [cardArray, setCardArray] = useState([]);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+    .then(response => response.json())
+    .then(data => {
+      setCardArray(() => data.results.slice(0, 12));
+    })
+    .catch(error => console.error(error))
+  }, []);
+
+  function changeArray() {
+    shuffleArray(cardArray);
+  }
+
+  function createCard(cardArray) {
+    return cardArray.map((data) => (
+      <>
+      <div id={data.id} className="card"><img src={data.image} onClick={() => changeArray}></img><p className="card-name">{data.name}</p></div>
+      </>
+    ))
+  }
+
+  function shuffleArray(array) {
+    let tempArray = array;
+    for (var i = tempArray.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = tempArray[i];
+        tempArray[i] = tempArray[j];
+        tempArray[j] = temp;
+        setCardArray(() => tempArray);
+    }
+}
+
   return (
     <>
+    <header>Memory Game</header>
+      <div className="score-main">
+        <div className="current-score">Current Score: 10</div>
+        <div className="highest-score">Highest Score: 12</div>
+      </div>
+    <section className="grid-main">
+      {createCard(cardArray)}
+    </section>
     </>
   )
 }
