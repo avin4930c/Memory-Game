a61be63e4ebd5b5c6b96f72619a79326cb44a2c6
Avinash <123456789+avinash4930c@gmail.com>
2023-12-15 18:18:43 +0530

    game logic completed and ux done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './App.css'
 function App() {
 
   const [cardArray, setCardArray] = useState([]);
+  const [currentArray, setCurrentArray] = useState([]);
+  let [score, setScore] = useState(0);
+  let [highScore, setHighScore] = useState(0);
+  let [gridColor, setGridColor] = useState("green");
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
@@ -14,37 +18,51 @@ function App() {
     .catch(error => console.error(error))
   }, []);
 
-  function changeArray() {
-    shuffleArray(cardArray);
+  function changeArray(id) {
+    if (!(currentArray.includes(id))) {
+      setCurrentArray((arr) => [...arr, id]);
+      setGridColor("green"); //to change right back after trigerring else condition(to click twice)
+      setScore(score => score += 1);
+      shuffleArray(cardArray);
+      
+    }
+    else {
+      setCurrentArray([]);
+      setGridColor("red");
+      setTimeout(() => setGridColor("green"), 1000);
+      setHighScore(prevScore => prevScore > score ? prevScore : score);
+      setScore(0);
+    }
   }
+  useEffect(() => {console.log(currentArray)}, [currentArray]);
 
   function createCard(cardArray) {
     return cardArray.map((data) => (
       <>
-      <div id={data.id} className="card"><img src={data.image} onClick={() => changeArray}></img><p className="card-name">{data.name}</p></div>
+      <div id={data.id} className="card" onClick={() => changeArray(data.id)}><img src={data.image}></img><p className="card-name">{data.name}</p></div>
       </>
     ))
   }
 
   function shuffleArray(array) {
-    let tempArray = array;
+    let tempArray = [...array];
     for (var i = tempArray.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = tempArray[i];
         tempArray[i] = tempArray[j];
         tempArray[j] = temp;
-        setCardArray(() => tempArray);
     }
+    setCardArray(tempArray);
 }
 
   return (
     <>
     <header>Memory Game</header>
       <div className="score-main">
-        <div className="current-score">Current Score: 10</div>
-        <div className="highest-score">Highest Score: 12</div>
+        <div className="current-score">Current Score: {score}</div>
+        <div className="highest-score">Highest Score: {highScore}</div>
       </div>
-    <section className="grid-main">
+    <section className="grid-main" style={{backgroundColor: gridColor}}>
       {createCard(cardArray)}
     </section>
     </>
